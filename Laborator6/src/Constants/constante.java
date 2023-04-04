e3353120a4118f554ba60416300f093cc5edc082
baicoianud422 <danielaionelia.baicoianu@fisglobal.com>
2023-04-04 17:42:53 +0300

    commit1

--- Laborator6/src/Constants/constante.java
+++ Laborator6/src/Constants/constante.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class constante {
-    public final String LISTA_ANIMALE_GOALA = "Niciun animal in lista";
+    public final String LISTA_ANIMALE_GOALA = "Niciun animal ";
 
     public static final String listavaccinurigoala = "Animalul nu are niciun vaccin";
 
