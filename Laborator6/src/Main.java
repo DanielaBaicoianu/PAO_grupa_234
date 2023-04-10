89eed8e2085bce8c8760ce05701fa46a93a48bcf
Ana <anaturcitu08@yahoo.com>
2023-04-10 09:15:09 +0300

    al doilea commit

--- Laborator6/src/Main.java
+++ Laborator6/src/Main.java
@@ -2,7 +2,7 @@ import model1.Caine;
 
 public class Main {
     public static void main(String[] args) {
-        Caine caine = new Caine("numeCaine", "labrador");
+        Caine caine = new Caine("Rex", "labrador");
 
         System.out.println(caine.vorbeste());
     }
